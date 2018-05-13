a7b043e5b07228d11497cd7df4fcb1c68e11675a
liyabing12138 <956825173@qq.com>
2018-05-13 20:28:28 +0800

    增加日志

--- blog/blog/blog-consumer/src/main/java/com/blog/wcl/consumer/ProviderApplication.java
+++ blog/blog/blog-consumer/src/main/java/com/blog/wcl/consumer/ProviderApplication.java
@@ -1,11 +1,12 @@
 package com.blog.wcl.consumer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import feign.Logger;
 
 
 /**
@@ -16,7 +17,16 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderApplication {
-
+	
+	@Bean
+	public Logger.Level feignLoggerLevel(){
+//		NONE, 不记录任何信息，默认值。
+//		BASIC, 记录请求方法、请求URL、状态码和用时。
+//		HEADERS, 在BASIC的基础上再记录一些常用信息。
+//		FULL: 记录请求和响应报文的全部内容。
+		return Logger.Level.FULL;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProviderApplication.class, args);
 	}
